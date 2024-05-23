cb936436ef81403f59a7dd9b3c0ca8be13e3abeb
Alex Kühne <alex@nordformat.ch>
2024-05-23 18:28:00 +0900

    Added de/index.html and en/index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
+import { resolve } from "path"
 import { defineConfig } from "vite"
+import handlebars from "vite-plugin-handlebars"
 
 export default defineConfig({
-  base: "./",
   server: {
     host: true,
   },
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        en: resolve(__dirname, "en/index.html"),
+        de: resolve(__dirname, "de/index.html"),
+      },
+    },
+  },
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, "partials"),
+      helpers: {
+        translate: (key) => key,
+      },
+    }),
+  ],
 })
